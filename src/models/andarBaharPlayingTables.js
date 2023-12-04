355e086de6fc8f2bb07b8101b17b19ed18707c18
AnilNikam <anilnikam619@gmail.com>
2023-12-04 21:44:56 +0530

    add ander bahar

---
+++ src/models/andarBaharPlayingTables.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const collectionName = 'andarBaharPlayingTables';
+const BetLists = mongoose.model('betList');
+
+const PlayingTablesSchema = new Schema({
+    gameId: { type: String, default: "" },
+    entryFee: { type: Number },
+    gameType: { type: String },
+    maxSeat: { type: Number, default: 6 },
+    activePlayer: { type: Number, default: 0 },
+    betId: { type: mongoose.Schema.Types.ObjectId, ref: BetLists },
+    boot: { type: Number, default: 0 },
+    chalLimit: { type: Number, default: 0 },
+    potLimit: { type: Number, default: 0 },
+    anderCards: [{ type: String, default: [] }],
+    baharCards: [{ type: String, default: [] }],
+    playerInfo: [],
+    gameState: { type: String, default: "" },
+    turnStartTimer: { type: Date },
+    dealerSeatIndex: { type: Number, default: -1 },
+    turnSeatIndex: { type: Number, default: -1 },
+    jobId: { type: String, default: "" },
+    turnDone: { type: Boolean, default: false },
+    gameTimer: {},
+    gameTracks: [],
+    callFinalWinner: { type: Boolean, default: false },
+    isLastUserFinish: { type: Boolean, default: false },
+    isFinalWinner: { type: Boolean, default: false },
+}, { versionKey: false });
+
+module.exports = mongoose.model(collectionName, PlayingTablesSchema, collectionName);
