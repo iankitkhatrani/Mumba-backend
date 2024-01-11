1a0d2c8c59a63de3b3d755d979cb72e78496a7a0
ankit <iankitkhatrani@gmail.com>
2024-01-11 10:14:06 +0530

    fix bug

--- src/SpinerGame/index.js
+++ src/SpinerGame/index.js
@@ -1,12 +1,11 @@
 
-const { getBetList } = require('./betList');
+
 const { SPINNER_JOIN_TABLE } = require("./joinTable");
 const { leaveTable } = require("./leaveTable");
 const { disconnectTableHandle, findDisconnectTable } = require("./disconnectHandle");
 const { cardPack, seeCard, chal, show } = require("./gamePlay");
 
 module.exports = {
-  getBetList: getBetList,
   SPINNER_JOIN_TABLE: SPINNER_JOIN_TABLE,
   cardPack: cardPack,
   seeCard: seeCard,
